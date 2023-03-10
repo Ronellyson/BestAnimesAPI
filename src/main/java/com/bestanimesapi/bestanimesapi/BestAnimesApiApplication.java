24cbaea3cca5192a493c76d5256a41945e34f4dc
Ronellyson <ronellysonjulio@gmail.com>
2023-03-10 16:56:10 -0300

    first commit

---
+++ src/main/java/com/bestanimesapi/bestanimesapi/BestAnimesApiApplication.java
@@ -0,0 +1,13 @@
+package com.bestanimesapi.bestanimesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestAnimesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestAnimesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestanimesapi/bestanimesapi/BestAnimesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestanimesapi.bestanimesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestAnimesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
